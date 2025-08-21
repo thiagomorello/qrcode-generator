89ab587b86b707a4031f70831fdd95aa230017fc
Thiago Roieski Maltezo <thiago@thiagomorello.com>
2025-08-21 11:15:30 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const QRCode = require('qrcode');
 const app = express();
-const port = 3000;
+const port = 8080;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
